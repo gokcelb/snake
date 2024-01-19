433f7bb7dd0deacd6e3c9bcdb0e67e194f5bd8d1
Gökçe Laçin Bilgin <lacin@thoughtbot.com>
2024-01-19 16:20:47 +0300

    Add food to random location
    Food is added to a random location on the map and collision with
    borders is handled by re-randomizing the location and re-trying
    the map update.

--- lib/snake.rb
+++ lib/snake.rb
@@ -5,6 +5,4 @@ width = 90
 
 game = Snake::Game.new
 game.create_map(height, width, with_visible_borders: true)
-# game.add_player
-# game.add_food_to_random_position
-# game.play
+game.add_food
--- lib/snake/game.rb
+++ lib/snake/game.rb
@@ -8,14 +8,6 @@ module Snake
       loop do
         player_input = $stdin.getch
         case player_input
-        when "w"
-          move_player(Direction::UP)
-        when "a"
-          move_player(Direction::LEFT)
-        when "s"
-          move_player(Direction::DOWN)
-        when "d"
-          move_player(Direction::RIGHT)
         when "q"
           break
         end
@@ -25,31 +17,23 @@ module Snake
     def create_map(height, width, with_visible_borders: false)
       @map = Map.new(height, width)
       @map.create_borders("|", "-") if with_visible_borders
-      @map.render
     end
 
-    def add_player(x = @map.width / 2, y = @map.height / 2)
-      @player = Player::Snake.new(x, y, "-", "|")
-      @map.update_tile_representation(x, y, @player.head.representation)
-      @map.render
-    end
+    def add_food
+      object = Objects::Food.new
+      index = Random.rand(1..@map.width * @map.height)
+      updated = false
 
-    def add_food_to_random_position
-      x = Random.rand(1..@map.width)
-      y = Random.rand(1..@map.height)
-      food_representation = "*"
-
-      @map.update_tile_representation(x, y, food_representation)
-      @map.render
-    end
-
-    def move_player(direction)
-      @map.update_tile_representation(@player.tail.x, @player.tail.y, " ")
-
-      @player.move(direction)
-
-      @map.update_tile_representation(@player.head.x, @player.head.y, @player.head.representation)
-      @map.render
+      until updated
+        begin
+          @map.update(index, object)
+          updated = true
+        rescue Map::CollisionError => exception
+          if exception.object_type == Object::Types::BORDER
+            index = Random.rand(1..@map.width * @map.height)
+          end
+        end
+      end
     end
   end
 end
